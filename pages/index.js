cc70db55e5e8aeb8892616ce358af27cd712055d
Craig Spaeth <craigspaeth@gmail.com>
2018-03-07 19:39:07 -0500

    Progress on hero & header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
+import Header from '../components/header'
+import Meta from '../components/meta'
+import Hero from '../components/hero'
 import initHeroViz from '../lib/hero-viz'
 
 export default class extends React.Component {
-  componentDidMount () {
-    initHeroViz()
-  }
-
   render () {
     return (
       <div>
-        Hello World
+        <Meta />
+        <Header />
+        <Hero />
+        <style jsx>
+          {`
+        div {
+          height: 1000px;
+        }
+      `}
+        </style>
       </div>
     )
   }
